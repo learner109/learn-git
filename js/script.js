a4c90a80aa9a353675c1cec8d4149bce3a4058db
Mohamed <developmentpro19@gmail.com>
2025-01-25 19:01:28 +0200

    Add Scroll To Top Button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+const showScrollPoint = 150;
+const scrollTopEle = document.querySelector(".scroll-top");
+
+const handleScrollAppearance = (e) => {
+  const scrollPosition = window.scrollY || window.pageYOffset;
+  if (scrollPosition <= 150) scrollTopEle.classList.remove("show");
+  else scrollTopEle.classList.add("show");
+};
+
+const handleScrollTop = () => {
+  window.scrollTo({
+    behavior: "smooth",
+    top: 0,
+  });
+};
+
+window.addEventListener("scroll", handleScrollAppearance);
+scrollTopEle.addEventListener("click", handleScrollTop);
